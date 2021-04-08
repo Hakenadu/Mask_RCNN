5989db3190f7702e028fe853b12a6ebc33a84556
Manuel <m.seiche@web.de>
2021-04-08 15:24:54 +0200

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2353,14 +2353,6 @@ class MaskRCNN(object):
         self.set_trainable(layers)
         self.compile(learning_rate, self.config.LEARNING_MOMENTUM)
 
-        # Work-around for Windows: Keras fails on Windows when using
-        # multiprocessing workers. See discussion here:
-        # https://github.com/matterport/Mask_RCNN/issues/13#issuecomment-353124009
-        if os.name == 'nt':
-            workers = 0
-        else:
-            workers = multiprocessing.cpu_count()
-
         self.keras_model.fit(
             train_generator,
             initial_epoch=self.epoch,
@@ -2370,8 +2362,8 @@ class MaskRCNN(object):
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
-            use_multiprocessing=workers > 1,
+            workers=1,
+            use_multiprocessing=False # https://github.com/matterport/Mask_RCNN/issues/287
         )
         self.epoch = max(self.epoch, epochs)
 
